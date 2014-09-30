f7a95bd2766dab5fd4967833b2d041fa1502c08b
T <T@PC269>
2014-09-29 19:47:31 -0500

    primer subida

---
+++ src/proyectosistemapago/Pago.java
@@ -0,0 +1,27 @@
+
+package proyectosistemapago;
+
+public class Pago  {
+    
+    public Pago(){
+System.out.println("se esta ejecutando");
+}
+    
+    
+    public float hacerPago(int horasTrabajadas, float sueldo){
+    float pago=horasTrabajadas*sueldo;
+    
+    
+       return pago;
+       
+    
+    }
+            
+         
+    
+ 
+    
+    
+    
+    
+}
---
+++ src/proyectosistemapago/ProyectoSistemaPago.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package proyectosistemapago;
+
+/**
+ *
+ * @author T
+ */
+public class ProyectoSistemaPago {
+
+ 
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Pago chamaco1=new Pago();
+        Pago chamaco2;
+        
+        System.out.print(new Pago(). hacerPago(20,3000));
+        
+        System.out.println(chamaco1.hacerPago(10,2000));
+        // TODO code application logic here
+    }
+    
+    
+}
